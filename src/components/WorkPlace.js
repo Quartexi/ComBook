051cb743a422f1df591be3e0c933a94413e3bcb3
Ament <Kevin.Ament@comdok.de>
2023-03-30 16:09:55 +0200

    booking workplaces made better - orange workplaces for indicating, that its yours - detailed view on all workplaces

--- src/components/WorkPlace.js
+++ src/components/WorkPlace.js
@@ -1,19 +1,21 @@
 import React, {useState} from 'react';
 import '../CSS/WorkPlace.css';
 
-const WorkPlace = ({sizeX, sizeY, row, column, id, bookingid, bookid}) => {
-    const [clicked, setClicked] = useState(false);
+const WorkPlace = ({sizeX, sizeY, row, column, id, bookingid, username, bookid, clicked, infoScreen}) => {
+
     const [isHovering, setIsHovering] = useState(false);
+    const usernameStorage = sessionStorage.getItem('username');
 
     const squareStyle = {
         width: `${sizeX}px`,
         height: `${sizeY}px`,
         gridRow: `${row} / span 1`,
         gridColumn: `${column} / span 1`,
-        backgroundColor: clicked ? 'green' : (bookingid > 0 ? 'red' : (isHovering ? '#CCCCCC' : '#FFFFFF')),
+        backgroundColor: clicked ? 'green' : (username === usernameStorage ? 'orange' : (bookingid > 0 ? 'red' : (isHovering ? '#CCCCCC' : '#FFFFFF'))),
         cursor: 'pointer',
         id: id,
-        bookingid: bookingid
+        bookingid: bookingid,
+        username: username
     };
 
     const handleMouseEnter = () => {
@@ -26,8 +28,11 @@ const WorkPlace = ({sizeX, sizeY, row, column, id, bookingid, bookid}) => {
 
     const handleClick = () => {
         if(bookingid === 0) {
-            setClicked(!clicked);
-            bookid(id);
+            bookid(id, clicked);
+        } else {
+            infoScreen(id, bookingid);
+            console.log(username);
+            console.log(usernameStorage);
         }
     };
 
--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -4,7 +4,8 @@ import WorkPlace from "../components/WorkPlace";
 import '../CSS/WorkPlace.css';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-import { useState} from "react";
+import {useState} from "react";
+import {Button} from "react-bootstrap";
 
 const BookingPage = () => {
     const navigate = useNavigate();
@@ -14,9 +15,18 @@ const BookingPage = () => {
     const username = sessionStorage.getItem('username');
     const [selectedOption, setSelectedOption] = useState();
     const [rooms, setRooms] = useState([]);
-    const [workPlaceId, setWorkPlaceId] = useState(0);
     const [date, setDate] = useState(new Date());
     const [dateString, setDateString] = useState(new Date().toLocaleDateString("de-DE"));
+
+    const [specialView, setSpecialView] = useState(false);
+    const [specialViewCreated, setSpecialViewCreated] = useState();
+    const [specialViewWorkplaceId, setSpecialViewWorkplaceId] = useState();
+    const [specialViewDate, setSpecialViewDate] = useState();
+    const [specialViewUsername, setSpecialViewUsername] = useState();
+    const [specialViewDelete, setSpecialViewDelete] = useState();
+
+
+    const [clickedChildId, setClickedChildId] = useState(null);
 
     function SetFloor(floor) {
         getRooms(floor).then(async (response) => {
@@ -40,6 +50,7 @@ const BookingPage = () => {
         await setDate(dateTemp);
         await setDateString(dateTemp.toLocaleDateString("de-DE"));
         await SetFloor(selectedOption);
+        await setClickedChildId(null);
     };
 
     const subtractDays = async () => {
@@ -48,6 +59,7 @@ const BookingPage = () => {
             await dateTemp.setDate(date.getDate() - 1);
             await setDate(dateTemp);
             await setDateString(dateTemp.toLocaleDateString("de-DE"));
+            await setClickedChildId(null);
             await SetFloor(selectedOption);
         }
     }
@@ -63,7 +75,7 @@ const BookingPage = () => {
                     'Access-Control-Allow-Methods': 'DELETE, GET, OPTIONS, PATCH, POST, PUT, FETCH',
                     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
                 },
-                body: JSON.stringify({floor: floor, workplaceList: [{date: date.toLocaleDateString("de-DE")}]})
+                body: JSON.stringify({floor: floor, workplaceList: [{date: date.toLocaleDateString("de-DE"), username: ''}]})
             }).then((response) => response.json());
         } catch (error) {
             console.log(error);
@@ -72,7 +84,7 @@ const BookingPage = () => {
 
     const bookWorkplace = async () => {
         try {
-            return await fetch('http://127.0.0.1:5071/api/Room/bookWorkplace', {
+            return await fetch('http://127.0.0.1:5071/api/Booking/bookWorkplace', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
@@ -81,27 +93,66 @@ const BookingPage = () => {
                     'Access-Control-Allow-Methods': 'DELETE, GET, OPTIONS, PATCH, POST, PUT, FETCH',
                     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
                 },
-                body: JSON.stringify({username: username, workplaceId: 1})
+                body: JSON.stringify({username: username, workplaceid: clickedChildId, date: date})
+            }).then((response) => {
+                response.json();
+                SetFloor(selectedOption);
+                setClickedChildId(null);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getBookingInformation = async (bookingid) => {
+        try {
+            return await fetch('http://127.0.0.1:5071/api/Booking/getBookingInformation', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'DELETE, GET, OPTIONS, PATCH, POST, PUT, FETCH',
+                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+                },
+                body: JSON.stringify({bookingid: bookingid, date: "", username: ""})
             }).then((response) => response.json());
         } catch (error) {
             console.log(error);
         }
     }
 
-    const bookid = (id) => {
-        if(workPlaceId === id) {
-            setWorkPlaceId(0);
-            //background color should go back to white from the one
+    const bookid = (childId, childClicked) => {
+        if (childClicked) {
+            // child was already clicked, so reset the clickedChildId state to null
+            setClickedChildId(null);
         } else {
-            setWorkPlaceId(id);
-            //backgroundcolor should switch to the otherone clicked
+            // child was not already clicked, so update the clickedChildId state to the new child's ID
+            setClickedChildId(childId);
         }
+    }
+
+    function infoScreen(id, bookingid) {
+        getBookingInformation(bookingid).then(async (response) => {
+            await setSpecialViewCreated(response.created);
+            await setSpecialViewWorkplaceId(response.workplaceid);
+            await setSpecialViewDate(response.date);
+            await setSpecialViewUsername(response.username);
+        })
+            .catch((e) => {
+                console.log(e.message);
+            })
+        if(username === specialViewUsername) {
+            setSpecialViewDelete(true);
+        } else {
+            setSpecialViewDelete(false);
+        }
+        setSpecialView(true);
     }
 
     return (
         <div>
             <h2>BookingPage</h2>
-
             <h3>
                 <button onClick={subtractDays}>zurück</button>
                 {date.toLocaleDateString("de-DE")}
@@ -125,8 +176,9 @@ const BookingPage = () => {
                                         {rooms[index].workplaceList.map((workplace) => (
                                             <WorkPlace sizeX={workplace.sizeX} sizeY={workplace.sizeY}
                                                        row={workplace.row} column={workplace.column} id={workplace.id}
-                                                       bookingid={workplace.bookingid}
-                                                       bookid={bookid}/>
+                                                       bookingid={workplace.bookingid} username={workplace.username}
+                                                       bookid={bookid} infoScreen={infoScreen}
+                                                       clicked={workplace.id === clickedChildId}/>
                                         ))}
                                     </div>
                                 </Room>
@@ -164,6 +216,30 @@ const BookingPage = () => {
                         </>
                     )}
                 </div>
+                {specialView === true && (
+                    <>
+                    <div>
+                        <h2>
+                            Erstellt Am: {specialViewCreated}
+                        </h2>
+                        <h2>
+                            Für das Datum: {specialViewDate}
+                        </h2>
+                        <h2>
+                            Sitzplatz: {specialViewWorkplaceId}
+                        </h2>
+                        <h2>
+                            Username: {specialViewUsername}
+                        </h2>
+                        <Button onClick={() => setSpecialView(false)}>Schließen</Button>
+                        {specialViewDelete === true && (
+                            <>
+                                <Button>Delete</Button>
+                            </>
+                        )}
+                    </div>
+                    </>
+                    )}
             </form>
         </div>
     );
