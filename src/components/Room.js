a5c542a28cfa00b9971f6a7684a766e8c2c139e6
Ament <Kevin.Ament@comdok.de>
2023-04-03 15:52:52 +0200

    workplaces and rooms improvement logic

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,10 +2,8 @@ import '../CSS/Room.css';
 
 const Room = ({children, sizeX, sizeY, row, column, id}) => {
     const squareStyle = {
-        width: `${sizeX}px`,
-        height: `${sizeY}px`,
-        gridRow: `${row} / span 1`,
-        gridColumn: `${column} / span 1`,
+        gridRow: `${row}`,
+        gridColumn: `${column}`,
         id: `${id}`,
     };
     return <div className="square" style={squareStyle}>
--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -21,6 +21,7 @@ const BookingPage = () => {
     const [specialView, setSpecialView] = useState(false);
     const [specialViewCreated, setSpecialViewCreated] = useState();
     const [specialViewWorkplaceId, setSpecialViewWorkplaceId] = useState();
+    const [specialViewBookingId, setSpecialViewBookingId] = useState();
     const [specialViewDate, setSpecialViewDate] = useState();
     const [specialViewUsername, setSpecialViewUsername] = useState();
     const [specialViewDelete, setSpecialViewDelete] = useState();
@@ -32,6 +33,7 @@ const BookingPage = () => {
         getRooms(floor).then(async (response) => {
             await setRooms(response);
             setSelectedOption(floor);
+            setSpecialView(false);
         })
             .catch((e) => {
                 console.log(e.message);
@@ -75,7 +77,12 @@ const BookingPage = () => {
                     'Access-Control-Allow-Methods': 'DELETE, GET, OPTIONS, PATCH, POST, PUT, FETCH',
                     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
                 },
-                body: JSON.stringify({floor: floor, workplaceList: [{date: date.toLocaleDateString("de-DE"), username: ''}]})
+                body: JSON.stringify({
+                    floor: floor,
+                    row: '',
+                    column: '',
+                    workplaceList: [{date: date.toLocaleDateString("de-DE"), username: ''}]
+                })
             }).then((response) => response.json());
         } catch (error) {
             console.log(error);
@@ -98,6 +105,26 @@ const BookingPage = () => {
                 response.json();
                 SetFloor(selectedOption);
                 setClickedChildId(null);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const deleteWorkplace = async () => {
+        try {
+            return await fetch('http://127.0.0.1:5071/api/Booking/deleteWorkplace', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'DELETE, GET, OPTIONS, PATCH, POST, PUT, FETCH',
+                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+                },
+                body: JSON.stringify({username: username, bookingid: specialViewBookingId, date: ''})
+            }).then((response) => {
+                SetFloor(selectedOption);
             });
         } catch (error) {
             console.log(error);
@@ -132,23 +159,27 @@ const BookingPage = () => {
         }
     }
 
-    function infoScreen(id, bookingid) {
-        getBookingInformation(bookingid).then(async (response) => {
+    async function infoScreen(id, bookingid) {
+        try {
+            const response = await getBookingInformation(bookingid);
+            await setSpecialViewBookingId(bookingid);
             await setSpecialViewCreated(response.created);
             await setSpecialViewWorkplaceId(response.workplaceid);
             await setSpecialViewDate(response.date);
             await setSpecialViewUsername(response.username);
-        })
-            .catch((e) => {
-                console.log(e.message);
-            })
-        if(username === specialViewUsername) {
-            setSpecialViewDelete(true);
-        } else {
-            setSpecialViewDelete(false);
+
+            if (username === response.username) {
+                setSpecialViewDelete(true);
+                setSpecialView(true);
+            } else {
+                setSpecialViewDelete(false);
+                setSpecialView(true);
+            }
+        } catch (e) {
+            console.log(e.message);
         }
-        setSpecialView(true);
     }
+
 
     return (
         <div>
@@ -161,86 +192,91 @@ const BookingPage = () => {
             <h2>
                 <button onClick={bookWorkplace}>Sitzplatz buchen</button>
             </h2>
-
-            <form onSubmit={handleLogout}>
-                <button type="submit">Logout</button>
-                <Dropdown options={options} value={selectedOption} placeholder="Wähle eine Etage"
-                          onChange={option => SetFloor(option.value)}/>
-                <div className="grid">
-                    {selectedOption === "1" && (
-                        <>
-                            {rooms.map((room, index) => (
-                                <Room sizeX={room.sizeX} sizeY={room.sizeY} column={room.column} row={room.row}
-                                      id={room.id}>
-                                    <div className="grid-workplace">
-                                        {rooms[index].workplaceList.map((workplace) => (
-                                            <WorkPlace sizeX={workplace.sizeX} sizeY={workplace.sizeY}
-                                                       row={workplace.row} column={workplace.column} id={workplace.id}
-                                                       bookingid={workplace.bookingid} username={workplace.username}
-                                                       bookid={bookid} infoScreen={infoScreen}
-                                                       clicked={workplace.id === clickedChildId}/>
-                                        ))}
-                                    </div>
-                                </Room>
-                            ))}
-                        </>
-                    )}
-                    {selectedOption === "2" && (
-                        <>
-                            {rooms.map((room, index) => (
-                                <Room sizeX={room.sizeX} sizeY={room.sizeY} column={room.column} row={room.row}
-                                      id={room.id}>
-                                    <div className="grid-workplace">
-                                        {rooms[index].workplaceList.map((workplace) => (
-                                            <WorkPlace sizeX={workplace.sizeX} sizeY={workplace.sizeY}
-                                                       row={workplace.row} column={workplace.column} id={workplace.id}/>
-                                        ))}
-                                    </div>
-                                </Room>
-                            ))}
-                        </>
-                    )}
-                    {selectedOption === "3" && (
-                        <>
-                            {rooms.map((room, index) => (
-                                <Room sizeX={room.sizeX} sizeY={room.sizeY} column={room.column} row={room.row}
-                                      id={room.id}>
-                                    <div className="grid-workplace">
-                                        {rooms[index].workplaceList.map((workplace) => (
-                                            <WorkPlace sizeX={workplace.sizeX} sizeY={workplace.sizeY}
-                                                       row={workplace.row} column={workplace.column} id={workplace.id}/>
-                                        ))}
-                                    </div>
-                                </Room>
-                            ))}
-                        </>
-                    )}
-                </div>
-                {specialView === true && (
-                    <>
-                    <div>
-                        <h2>
-                            Erstellt Am: {specialViewCreated}
-                        </h2>
-                        <h2>
-                            Für das Datum: {specialViewDate}
-                        </h2>
-                        <h2>
-                            Sitzplatz: {specialViewWorkplaceId}
-                        </h2>
-                        <h2>
-                            Username: {specialViewUsername}
-                        </h2>
-                        <Button onClick={() => setSpecialView(false)}>Schließen</Button>
-                        {specialViewDelete === true && (
+            <div style={{justifyContent: "center"}}>
+                <form onSubmit={handleLogout} style={{justifySelf: "center"}}>
+                    <button type="submit">Logout</button>
+                    <Dropdown options={options} value={selectedOption} placeholder="Wähle eine Etage"
+                              onChange={option => SetFloor(option.value)}/>
+                    <h1 style={{paddingLeft: "30%"}}>Eingang</h1>
+                    <div className="grid" style={{width: "90%"}}>
+                        {selectedOption === "1" && (
                             <>
-                                <Button>Delete</Button>
+                                {rooms.map((room, index) => (
+                                    <Room sizeX={room.sizeX} sizeY={room.sizeY} column={room.column} row={room.row}
+                                          id={room.id}>
+                                        <div className="grid-workplace">
+                                            {rooms[index].workplaceList.map((workplace) => (
+                                                <WorkPlace sizeX={workplace.sizeX} sizeY={workplace.sizeY}
+                                                           row={workplace.row} column={workplace.column}
+                                                           id={workplace.id}
+                                                           bookingid={workplace.bookingid} username={workplace.username}
+                                                           bookid={bookid} infoScreen={infoScreen}
+                                                           clicked={workplace.id === clickedChildId}/>
+                                            ))}
+                                        </div>
+                                    </Room>
+                                ))}
+                            </>
+                        )}
+                        {selectedOption === "2" && (
+                            <>
+                                {rooms.map((room, index) => (
+                                    <Room sizeX={room.sizeX} sizeY={room.sizeY} column={room.column} row={room.row}
+                                          id={room.id}>
+                                        <div className="grid-workplace">
+                                            {rooms[index].workplaceList.map((workplace) => (
+                                                <WorkPlace sizeX={workplace.sizeX} sizeY={workplace.sizeY}
+                                                           row={workplace.row} column={workplace.column}
+                                                           id={workplace.id}/>
+                                            ))}
+                                        </div>
+                                    </Room>
+                                ))}
+                            </>
+                        )}
+                        {selectedOption === "3" && (
+                            <>
+                                {rooms.map((room, index) => (
+                                    <Room sizeX={room.sizeX} sizeY={room.sizeY} column={room.column} row={room.row}
+                                          id={room.id}>
+                                        <div className="grid-workplace">
+                                            {rooms[index].workplaceList.map((workplace) => (
+                                                <WorkPlace sizeX={workplace.sizeX} sizeY={workplace.sizeY}
+                                                           row={workplace.row} column={workplace.column}
+                                                           id={workplace.id}/>
+                                            ))}
+                                        </div>
+                                    </Room>
+                                ))}
                             </>
                         )}
                     </div>
-                    </>
+                    {specialView === true && (
+                        <>
+                            <div>
+                                <h2>
+                                    Erstellt Am: {specialViewCreated}
+                                </h2>
+                                <h2>
+                                    Für das Datum: {specialViewDate}
+                                </h2>
+                                <h2>
+                                    Sitzplatz: {specialViewWorkplaceId}
+                                </h2>
+                                <h2>
+                                    Username: {specialViewUsername}
+                                </h2>
+                                <Button onClick={() => setSpecialView(false)}>Schließen</Button>
+                                {specialViewDelete === true && (
+                                    <>
+                                        <Button onClick={deleteWorkplace}>Delete</Button>
+                                    </>
+                                )}
+                            </div>
+                        </>
                     )}
-            </form>
+                </form>
+            </div>
         </div>
     );
 }
